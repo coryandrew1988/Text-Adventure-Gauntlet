d32b559bd0131259da9061b0ff35b91057670e3e
Cory <cory.andrew.1988@gmail.com>
2017-03-17 01:43:00 -0400

    upgraded react-native

--- TextAdventureGauntlet/android/app/src/main/java/com/textadventuregauntlet/MainApplication.java
+++ TextAdventureGauntlet/android/app/src/main/java/com/textadventuregauntlet/MainApplication.java
@@ -1,7 +1,6 @@
 package com.textadventuregauntlet;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import io.realm.react.RealmReactPackage;
