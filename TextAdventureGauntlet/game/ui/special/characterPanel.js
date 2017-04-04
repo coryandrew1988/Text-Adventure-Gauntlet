9a9c1e6e737f2370f83fdd15b051413680d3b25a
Cory <cory.andrew.1988@gmail.com>
2017-04-04 13:52:08 -0400

    animation tweaks; gray-out defeated characters

--- TextAdventureGauntlet/game/ui/special/characterPanel.js
+++ TextAdventureGauntlet/game/ui/special/characterPanel.js
@@ -7,30 +7,21 @@ import {
 } from '../basics';
 
 export default class CharacterPanel extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      shakeMagnitude: new Animated.ValueXY({ x: 0, y: 0 }),
-      flashValue: new Animated.Value(0)
-    };
-  }
-
   shake(x, y) {
-    this.setState({
-      shakeMagnitude: new Animated.ValueXY({ x, y })
-    });
-
-    return Animated.spring(
-      this.state.shakeMagnitude,
-      {
+    return Animated.sequence([
+      Animated.timing(this.state.shakeMagnitude, {
+        duration: 250,
+        toValue: { x, y },
+        easing: Easing.linear
+      }),
+      Animated.spring(this.state.shakeMagnitude, {
         toValue: { x: 0, y: 0 },
-        friction: 1.5
-      }
-    );
+        friction: 1.75
+      })
+    ]);
   }
 
-  flash(duration) {
+  flash(duration, endOnGray) {
     return Animated.sequence([
       Animated.timing(this.state.flashValue, {
         duration: duration / 2,
@@ -39,49 +30,66 @@ export default class CharacterPanel extends Component {
       }),
       Animated.timing(this.state.flashValue, {
         duration: duration / 2,
-        toValue: 0,
+        toValue: endOnGray ? -1 : 0,
         easing: Easing.out(Easing.linear)
       })
     ]);
   }
 
-  subscribe(system, character) {
+  cleanup() {
     if (this.subscription) {
       this.subscription.dispose();
     }
+  }
+
+  initialize(system, character) {
+    this.cleanup();
+
+    this.setState({
+      shakeMagnitude: new Animated.ValueXY({ x: 0, y: 0 }),
+      flashValue: new Animated.Value(character.stats.hp === 0 ? -1 : 0)
+    });
 
     this.subscription = system.world.events.subscribe('damage', {
       targetId: character.id
-    }, () => {
+    }, ({ damage, becameDefeated }) => {
+      const damageMagnitude = (
+        becameDefeated ?
+          1 :
+          damage / character.stats.maxHP
+      );
       const shakeAngle = Math.random() * Math.PI * 2;
-      Animated.parallel([
-        this.shake(8 * Math.cos(shakeAngle), 8 * Math.sin(shakeAngle)),
-        this.flash(200)
-      ]).start();
+      let animation = Animated.parallel([
+        this.shake(
+          damageMagnitude * 16 * Math.cos(shakeAngle),
+          damageMagnitude * 16 * Math.sin(shakeAngle)
+        ),
+        this.flash(200, becameDefeated)
+      ]);
+
+      animation.start()
     });
   }
 
   componentWillMount() {
-    this.subscribe(this.props.system, this.props.character);
+    this.initialize(this.props.system, this.props.character);
   }
 
   componentWillUnmount() {
-    if (this.subscription) {
-      this.subscription.dispose();
-    }
+    this.cleanup();
   }
 
   componentWillReceiveProps(nextProps) {
     if (this.props.character.id === nextProps.character.id) { return; }
 
-    this.subscribe(nextProps.system, nextProps.character);
+    this.initialize(nextProps.system, nextProps.character);
   }
 
   render() {
     const { style } = this.props;
     const animatedBackgroundColor = this.state.flashValue.interpolate({
-      inputRange: [0, 1],
-      outputRange: [style.backgroundColor, '#fff']
+      inputRange: [-1, 0, 1],
+      outputRange: ['#444', style.backgroundColor || '#000', '#fff']
     });
 
     return <AnimatedPanel style={[style, {
