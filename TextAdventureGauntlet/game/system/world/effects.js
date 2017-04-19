c0030a9e1cfd78765951c6420c548e4cfc0dabbf
Cory <cory.andrew.1988@gmail.com>
2017-04-19 19:48:15 -0400

    allowed reversible effect logic

--- TextAdventureGauntlet/game/system/world/effects.js
+++ TextAdventureGauntlet/game/system/world/effects.js
@@ -1,27 +1,45 @@
 export const createEffectSystem = () => {
-  const map = new Map();
+  const forwardMap = new Map();
+  const reverseMap = new Map();
 
-  const execute = (effect, context) => {
-    if (!effect) { return null; }
+  const createExecuteFunction = (map, onMissing) => {
+    const execute = (effect, context) => {
+      if (!effect) { return null; }
 
-    if (Array.isArray(effect)) {
-      return effect.map((e) => {
-        return execute(e, context);
-      });
-    }
+      if (Array.isArray(effect)) {
+        return effect.map((e) => {
+          return execute(e, context);
+        });
+      }
 
-    const effectCallback = map.get(effect.key);
-    if (!effectCallback) {
-      throw new Error(`There is no effect with key "${effect.key}".`);
-    }
+      const effectCallback = map.get(effect.key);
+      if (!effectCallback) {
+        onMissing(effect, context);
+      }
 
-    return effectCallback(effect.params, context);
+      return effectCallback(effect.params, context);
+    };
+
+    return execute;
   };
 
+
+  const execute = createExecuteFunction(forwardMap, (effect, context) => {
+    throw new Error(`There is no effect with key "${effect.key}".`);
+  });
+  const reverse = createExecuteFunction(reverseMap, (effect, context) => {
+    throw new Error(`There is no reversible effect with key "${effect.key}".`);
+  });
+
   return {
-    define: (key, callback) => {
-      map.set(key, callback);
+    define: (key, callback, { reverse } = {}) => {
+      forwardMap.set(key, callback);
+
+      if (reverse) {
+        reverseMap.set(key, reverse);
+      }
     },
-    execute
+    execute,
+    reverse
   };
 };
